144dd15a8ea2879a60287ebd395a60453c71ccb4
Amarjot singh <amarjotsingh90@yahoo.com>
2017-10-26 01:46:32 -0700

    layout for the setting page is designed

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,6 +11,8 @@
 
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
+    <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+
     <link rel="stylesheet" href="{{ URL::to('css/app.css') }}">
 </head>
 <body>
@@ -25,6 +27,9 @@
         integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
         crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+
 <script src="{{ URL::to('js/app.js') }}"></script>
 
 </body>
--- resources/views/users/settings.blade.php
+++ resources/views/users/settings.blade.php
@@ -6,6 +6,89 @@
 
 
 @section('body-content')
-    this is the setting page.
+
+    <div class="container-fluid">
+        <div class="row">
+
+    <nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar ">
+        <ul class="nav nav-pills flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Reports</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Analytics</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Export</a>
+            </li>
+        </ul>
+
+        <ul class="nav nav-pills flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nav item</a>
+            </li>
+
+        </ul>
+    </nav>
+
+
+    <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
+        <h1 class="text-center">Dashboard</h1>
+
+        <section class="row text-center placeholders">
+            <div class="col-md-12  placeholder">
+                <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle img-circle" alt="Generic placeholder thumbnail">
+                <h4>username</h4>
+                <div class="text-muted">bio</div>
+            </div>
+
+        </section>
+
+        <h2 class="text-center">Section title</h2>
+        <div class="table-responsive text-center">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th class="text-center">Setting</th>
+                    <th class="text-center">State</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Setting 1</td>
+                    <td><input type="checkbox" checked data-toggle="toggle" data-onstyle="success" data-offstyle="danger">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Setting 1</td>
+                    <td><input type="checkbox" checked data-toggle="toggle" data-onstyle="success" data-offstyle="danger"></td>
+                </tr>
+                <tr>
+                    <td>Setting 1</td>
+                    <td><input type="checkbox" checked data-toggle="toggle" data-onstyle="success" data-offstyle="danger"></td>
+                </tr>
+                <tr>
+                    <td>Setting 1</td>
+                    <td><input type="checkbox" checked data-toggle="toggle" data-onstyle="success" data-offstyle="danger"></td>
+                </tr>
+                <tr>
+                    <td>Setting 1</td>
+                    <td><input type="checkbox" checked data-toggle="toggle" data-onstyle="success" data-offstyle="danger"></td>
+                </tr>
+                <tr>
+                    <td>Setting 1</td>
+                    <td><input type="checkbox" checked data-toggle="toggle" data-onstyle="success" data-offstyle="danger"></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
+
+        </div>
+    </div>
+
     @endsection
 
